a34d3e36fbf3dbdfd313be44526cacb02a4cb0bf
George Chaniotakis <haniotinho@gmail.com>
2019-02-27 18:38:05 +0200

    Book class/model and controller created

---
+++ WebApiDemo/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApiDemo.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Title { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public int PublicationYear { get; set; }
+    }
+}
